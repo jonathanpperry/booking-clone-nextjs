7900a521d5f3eecd3ed96d312ec50d39fd0ffa40
Jonathan Perry <jpper@umich.edu>
2024-05-06 01:07:06 -0500

    Initialized nextjs app and install/init shadcn

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Booking.com Clone",
+  description: "Booking clone created with next framework",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {/* Header */}
+        
+        {children}
+      </body>
+    </html>
+  );
+}
