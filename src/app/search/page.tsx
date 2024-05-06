187fa9df13c18a8eafafca806566bcf1af39f349
Jonathan Perry <jpper@umich.edu>
2024-05-06 16:49:28 -0500

    Built the search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,25 @@
-function SearchPage() {
+import { notFound } from "next/navigation";
+
+type Props = {
+  searchParams: SearchParams;
+};
+
+type SearchParams = {
+  url: URL;
+  group_adults: string;
+  group_children: string;
+  no_rooms: string;
+  checkin: string;
+  checkout: string;
+};
+
+async function SearchPage({ searchParams }: Props) {
+  if (!searchParams.url) return notFound();
+  //   console.log(searchParams);
+  const results = await fetchResults(searchParams);
+
+  if (!results) return <div>No results...</div>;
+
   return <div>SearchPage</div>;
 }
 
