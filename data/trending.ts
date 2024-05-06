d8de68a37a956186c937b4a8db54d11fb7193336
Jonathan Perry <jpper@umich.edu>
2024-05-06 12:08:16 -0500

    Built the rest of the page

---
+++ data/trending.ts
@@ -0,0 +1,37 @@
+export const trending_data = [
+    {
+        id: 1,
+        src: "",
+        title: "Dubai",
+        location: "United Arab Emirates",
+        description: "15 Deals",
+      },
+      {
+        id: 2,
+        src: "",
+        title: "South Korea",
+        location: "Asia",
+        description: "32 Deals",
+      },
+      {
+        id: 3,
+        src: "",
+        title: "London",
+        location: "United Kingdom",
+        description: "45 Deals",
+      },
+      {
+        id: 4,
+        src: "",
+        title: "Singapore",
+        location: "Asia",
+        description: "88 Deals",
+      },
+      {
+        id: 5,
+        src: "",
+        title: "New York City",
+        location: "United States of America",
+        description: "92 Deals",
+      },
+    ];
