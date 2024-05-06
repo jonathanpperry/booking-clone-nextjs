4008c623265aa835e279eb36d49b2e22b3eef408
Jonathan Perry <jpper@umich.edu>
2024-05-06 10:55:46 -0500

    Built the Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {/* Header */}
-        
+        <Header />
+
         {children}
       </body>
     </html>
